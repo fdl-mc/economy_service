5d2002a9e741168d0dacf0ffcdb79f5e9b7749cf
arslee <me@arslee.dev>
2022-02-18 18:31:19 +0900

    feat: Implement Transaction modekl

--- economy_service/api/models.py
+++ economy_service/api/models.py
@@ -11,3 +11,16 @@ class EconomyStates(ormar.Model):
 
     id: int = ormar.Integer(primary_key=True)
     balance: int = ormar.Integer(default=0)
+
+
+class Transaction(ormar.Model):
+    class Meta:
+        tablename: str = "transactions"
+        database = database
+        metadata = metadata
+
+    id: int = ormar.Integer(primary_key=True, autoincrement=True)
+    payer_id: int = ormar.Integer()
+    payee_id: int = ormar.Integer()
+    amount: int = ormar.Integer()
+    comment: str = ormar.String(max_length=512, nullable=True)
