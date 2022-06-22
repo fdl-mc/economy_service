a67ee4296b240dcdd60cc27bb92257783b485a06
arslee <mail@arslee.me>
2022-06-22 13:40:13 +0900

    feat(service): implement deposit method

--- src/service.rs
+++ src/service.rs
@@ -138,9 +138,78 @@ impl EconomyServiceTrait for EconomyService {
 
     async fn deposit(
         &self,
-        _request: Request<DepositRequest>,
+        request: Request<DepositRequest>,
     ) -> Result<Response<DepositReply>, Status> {
-        Err(Status::unimplemented(""))
+        let message = request.get_ref();
+
+        // Validate request data
+        if !message.amount.is_positive() {
+            return Err(Status::invalid_argument(
+                "Amount should be a positive number",
+            ));
+        }
+
+        // Extract token from metadata
+        let token = match request.metadata().get("x-token") {
+            Some(res) => res.to_str().unwrap().to_string(),
+            None => return Err(Status::unauthenticated("No token provided")),
+        };
+
+        let mut users_client = self.users_client.clone();
+
+        // Fetch banker user data
+        let mut user_request = Request::new(GetSelfUserRequest {});
+        user_request
+            .metadata_mut()
+            .append("x-token", token.parse().unwrap());
+
+        let banker_user = match users_client.get_self_user(user_request).await {
+            Ok(res) => res.into_inner().user.unwrap(),
+            // TODO: handle it better
+            Err(err) => return Err(err),
+        };
+
+        // Fetch banker state
+        let banker_state = match economy_state::Entity::find()
+            .filter(economy_state::Column::UserId.eq(banker_user.id))
+            .one(&self.conn)
+            .await
+        {
+            Ok(res) => match res {
+                Some(res) => res,
+                None => return Err(Status::not_found("State not found")),
+            },
+            Err(err) => return Err(Status::internal(err.to_string())),
+        };
+
+        // Check whether the banker actually has banker permissions
+        if !banker_state.banker {
+            return Err(Status::permission_denied("You have no banker permissions"));
+        }
+
+        // Fetch user state
+        let user_state = match economy_state::Entity::find()
+            .filter(economy_state::Column::UserId.eq(message.user_id))
+            .one(&self.conn)
+            .await
+        {
+            Ok(res) => match res {
+                Some(res) => res,
+                None => return Err(Status::not_found("State not found")),
+            },
+            Err(err) => return Err(Status::internal(err.to_string())),
+        };
+
+        // Update their state
+        let mut user_state: economy_state::ActiveModel = user_state.into();
+        user_state.balance = Set(user_state.balance.unwrap() + message.amount);
+        match user_state.update(&self.conn).await {
+            Ok(_) => (),
+            Err(err) => return Err(Status::internal(err.to_string())),
+        };
+
+        // Prepare reply
+        Ok(Response::new(DepositReply {}))
     }
 
     async fn withdraw(
