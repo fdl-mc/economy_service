daad864257f6972a975c31caa532afc57a93ea38
arslee07 <mail@arslee.me>
2022-10-30 22:20:05 +0900

    implement core

---
+++ core/src/lib.rs
@@ -0,0 +1,53 @@
+use economy_service_entity::economy_state;
+use sea_orm::*;
+
+type DbResult<T> = Result<T, DbErr>;
+
+#[derive(Default, Copy, Clone)]
+pub struct UpdateEconomyStateForm {
+    pub balance: Option<i32>,
+    pub banker: Option<bool>,
+}
+
+pub async fn get_or_create_economy_state(
+    user_id: i32,
+    conn: &DbConn,
+) -> DbResult<economy_state::Model> {
+    match economy_state::Entity::find()
+        .filter(economy_state::Column::UserId.eq(user_id))
+        .one(conn)
+        .await?
+    {
+        Some(res) => Ok(res),
+        None => {
+            economy_state::ActiveModel {
+                user_id: Set(user_id),
+                ..Default::default()
+            }
+            .insert(conn)
+            .await
+        }
+    }
+}
+
+pub async fn update_economy_state(
+    user_id: i32,
+    form: UpdateEconomyStateForm,
+    conn: &DbConn,
+) -> DbResult<economy_state::Model> {
+    let mut state: economy_state::ActiveModel = economy_state::Entity::find()
+        .filter(economy_state::Column::UserId.eq(user_id))
+        .one(conn)
+        .await?
+        .ok_or(DbErr::RecordNotFound("Not found".to_owned()))
+        .map(Into::into)?;
+
+    if form.balance.is_some() {
+        state.balance = Set(form.balance.unwrap());
+    }
+    if form.banker.is_some() {
+        state.banker = Set(form.banker.unwrap());
+    }
+
+    state.update(conn).await
+}
